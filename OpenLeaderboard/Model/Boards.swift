2c9b5bffe717aba29ab8c35e74be895a0a9cfac3
Jason Lyster <36202406+lysterjason@users.noreply.github.com>
2020-10-26 20:27:58 -0600

    Initial Boards Screen and Nav Bar

---
+++ OpenLeaderboard/Model/Boards.swift
@@ -0,0 +1,16 @@
+//
+//  Boards.swift
+//  OpenLeaderboard
+//
+//  Created by Jason Lyster on 2020-10-24.
+//  Copyright © 2020 OpenLeaderboard. All rights reserved.
+//
+
+import Foundation
+
+struct Board: Codable {
+    var boardName: String
+    var description: String
+    var publicBoard: Bool
+    
+}
--- OpenLeaderboard/OpenLeaderboard/ContentView.swift
+++ OpenLeaderboard/OpenLeaderboard/ContentView.swift
@@ -8,8 +8,11 @@
 
 import SwiftUI
 let bgColor = Color(red: 94.0/255.0, green: 92.0/255.0, blue: 230.0/255.0, opacity: 1.0)
-
 let btnColor = Color(red: 249.0/255.0, green: 238.0/255.0, blue: 230.0/255.0, opacity: 1.0)
+let bronze = Color(red: 221.0/255.0, green: 167.0/255.0, blue: 123.0/255.0, opacity: 1.0)
+let silver = Color(red: 198.0/255.0, green: 185.0/255.0, blue: 169.0/255.0, opacity: 1.0)
+let gold = Color(red: 255.0/255.0, green: 191.0/255.0, blue: 0.0/255.0, opacity: 1.0)
+let platinum = Color(red: 152.0/255.0, green: 193.0/255.0, blue: 217.0/255.0, opacity: 1.0)
 
 struct ContentView: View {
     
@@ -136,8 +139,98 @@ struct Register: View {
         print("I am signed up!")
     }
 }
+
+struct TabParent: View {
+    var body: some View {
+        TabView {
+            ProfileView()
+             .tabItem {
+                Image(systemName: "person.fill")
+                Text("Profile")
+              }
+            MainBoardsView()
+              .tabItem {
+                 Image(systemName: "list.bullet")
+                 Text("Boards")
+               }
+            AddBoardView()
+              .tabItem {
+                 Image(systemName: "plus")
+                 Text("Add Board")
+               }
+            SearchView()
+              .tabItem {
+                 Image(systemName: "magnifyingglass")
+                 Text("Search")
+               }
+            NotificationsView()
+              .tabItem {
+                 Image(systemName: "bell.fill")
+                 Text("Notifications")
+               }
+           
+        }.accentColor(bgColor)
+    }
+}
+
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile Screen")
+    }
+}
+
+struct MainBoardsView: View {
+    
+    @State private var menuIndex: Int = 0
+    var body: some View {
+        VStack {
+            Picker("Boards Index", selection: $menuIndex) {
+                Text("All Boards")
+                Text("My Boards")
+            }.pickerStyle(SegmentedPickerStyle())
+            Spacer()
+            List {
+                HStack {
+                    Image(systemName: "seal.fill").foregroundColor(platinum)
+                    VStack (alignment: .leading) {
+                        Text("Joel's Board")
+                        Text("#1 of 56")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                    }.padding(EdgeInsets(top: 5, leading: 10, bottom: 5, trailing: 105))
+                    VStack (alignment: .trailing){
+                        Text("2100")
+                        Text("57W / 10L")
+                            .font(.system(size: 15))
+                            .foregroundColor(.gray)
+                    }
+                    Image(systemName: "chevron.right").padding(EdgeInsets(top: 0, leading: 27, bottom: 0, trailing: 0)).foregroundColor(.gray)
+                }
+            }
+        }
+    }
+}
+
+struct AddBoardView: View {
+    var body: some View {
+        Text("Add Boards Screen")
+    }
+}
+
+struct SearchView: View {
+    var body: some View {
+        Text("Search Screen")
+    }
+}
+
+struct NotificationsView: View {
+    var body: some View {
+        Text("Notifications Screen")
+    }
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        Register()
+        TabParent()
     }
 }
