f35519cd147303a7f1595119dc636148373f403b
Jason Lyster <36202406+lysterjason@users.noreply.github.com>
2020-10-29 19:49:46 -0600

    Try and navigate on success

---
+++ OpenLeaderboard/ViewModels/TabViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  TabViewModel.swift
+//  OpenLeaderboard
+//
+//  Created by Tim Lyster on 2020-10-29.
+//  Copyright © 2020 OpenLeaderboard. All rights reserved.
+//
+
+import Foundation
+
+class TabViewModel: ObservableObject, Codable {
+    
+    init() { }
+    
+
+}
